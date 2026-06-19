02a6b108d98f9aa82992210579360a647e985619
rupamsen02 <rupamsen195@gmail.com>
2026-06-20 00:54:44 +0530

    updated project

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", process.env.FRONTEND_URL],
+    origin: "https://freelance-platform-frontend.vercel.app",
     credentials: true,
   },
 });
@@ -50,7 +50,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", process.env.FRONTEND_URL],
+    origin: "https://freelance-platform-frontend.vercel.app",
     credentials: true,
   }),
 );
